5a57e6357eaac86ffa834a7abb349bcbeb400c97
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2025-03-30 23:58:29 +0900

    UnmarshalJSONのパターン追加

--- types_test.go
+++ types_test.go
@@ -644,3 +644,670 @@ func Test_MarshalJSON_NullBool(t *testing.T) {
 		t.Logf("expected: %v, actual: %v", expected, string(actual))
 	}
 }
+
+func Test_UnmarshalJSON_ValidString(t *testing.T) {
+	type Test struct {
+		Value nullable.String `json:"value"`
+	}
+
+	input := []byte(`{"value":"test value"}`)
+
+	var expected = Test{
+		Value: nullable.NewString("test value"),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullString(t *testing.T) {
+	type Test struct {
+		Value nullable.String `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullString(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidInt(t *testing.T) {
+	type Test struct {
+		Value nullable.Int `json:"value"`
+	}
+
+	input := []byte(`{"value":-123}`)
+
+	var expected = Test{
+		Value: nullable.NewInt(-123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullInt(t *testing.T) {
+	type Test struct {
+		Value nullable.Int `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullInt(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidUInt(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt `json:"value"`
+	}
+
+	input := []byte(`{"value":123}`)
+
+	var expected = Test{
+		Value: nullable.NewUInt(123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullUInt(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullUInt(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidInt8(t *testing.T) {
+	type Test struct {
+		Value nullable.Int8 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123}`)
+
+	var expected = Test{
+		Value: nullable.NewInt8(-123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullInt8(t *testing.T) {
+	type Test struct {
+		Value nullable.Int8 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullInt8(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidUInt8(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt8 `json:"value"`
+	}
+
+	input := []byte(`{"value":123}`)
+
+	var expected = Test{
+		Value: nullable.NewUInt8(123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullUInt8(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt8 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullUInt8(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidInt16(t *testing.T) {
+	type Test struct {
+		Value nullable.Int16 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123}`)
+
+	var expected = Test{
+		Value: nullable.NewInt16(-123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullInt16(t *testing.T) {
+	type Test struct {
+		Value nullable.Int16 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullInt16(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidUInt16(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt16 `json:"value"`
+	}
+
+	input := []byte(`{"value":123}`)
+
+	var expected = Test{
+		Value: nullable.NewUInt16(123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullUInt16(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt16 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullUInt16(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidInt32(t *testing.T) {
+	type Test struct {
+		Value nullable.Int32 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123}`)
+
+	var expected = Test{
+		Value: nullable.NewInt32(-123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullInt32(t *testing.T) {
+	type Test struct {
+		Value nullable.Int32 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullInt32(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidUInt32(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt32 `json:"value"`
+	}
+
+	input := []byte(`{"value":123}`)
+
+	var expected = Test{
+		Value: nullable.NewUInt32(123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullUInt32(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt32 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullUInt32(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidInt64(t *testing.T) {
+	type Test struct {
+		Value nullable.Int64 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123}`)
+
+	var expected = Test{
+		Value: nullable.NewInt64(-123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullInt64(t *testing.T) {
+	type Test struct {
+		Value nullable.Int64 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullInt64(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidUInt64(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt64 `json:"value"`
+	}
+
+	input := []byte(`{"value":123}`)
+
+	var expected = Test{
+		Value: nullable.NewUInt64(123),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullUInt64(t *testing.T) {
+	type Test struct {
+		Value nullable.UInt64 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullUInt64(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidFloat32(t *testing.T) {
+	type Test struct {
+		Value nullable.Float32 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123.456}`)
+
+	var expected = Test{
+		Value: nullable.NewFloat32(-123.456),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullFloat32(t *testing.T) {
+	type Test struct {
+		Value nullable.Float32 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullFloat32(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidFloat64(t *testing.T) {
+	type Test struct {
+		Value nullable.Float64 `json:"value"`
+	}
+
+	input := []byte(`{"value":-123.456}`)
+
+	var expected = Test{
+		Value: nullable.NewFloat64(-123.456),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullFloat64(t *testing.T) {
+	type Test struct {
+		Value nullable.Float64 `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullFloat64(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidBool_True(t *testing.T) {
+	type Test struct {
+		Value nullable.Bool `json:"value"`
+	}
+
+	input := []byte(`{"value":true}`)
+
+	var expected = Test{
+		Value: nullable.NewBool(true),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_ValidBool_False(t *testing.T) {
+	type Test struct {
+		Value nullable.Bool `json:"value"`
+	}
+
+	input := []byte(`{"value":false}`)
+
+	var expected = Test{
+		Value: nullable.NewBool(false),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
+
+func Test_UnmarshalJSON_NullBool(t *testing.T) {
+	type Test struct {
+		Value nullable.Bool `json:"value"`
+	}
+
+	input := []byte(`{"value":null}`)
+
+	var expected = Test{
+		Value: nullable.NewNullBool(),
+	}
+
+	var actual Test
+	err := json.Unmarshal(input, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !expected.Value.Equal(actual.Value) {
+		t.Fail()
+		t.Logf("expected: %v, actual: %v", expected.Value, actual.Value)
+	}
+}
